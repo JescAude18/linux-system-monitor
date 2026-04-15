362d9c370651f1c53d1109a23d66d002dfe2a6a8
JescAude <jessica.moussougan@epitech.eu>
2026-04-15 17:21:36 +0100

    feat :sparkles: : added displaying of disk usage (total, used, available)

--- server-stats.sh
+++ server-stats.sh
@@ -16,9 +16,19 @@ echo "-----RAM USAGE--------"
 echo -n "Total : "
 RAM_TOTAL=$(top -b -n 1 | grep "MiB Mem" | cut -d ',' -f1 | awk '{print $4}')
 echo "${RAM_TOTAL} MiB"
-echo -n "Free : "
-RAM_FREE=$(top -b -n 1 | grep "MiB Mem" | cut -d ',' -f2 | awk '{print $1}')
-echo "${RAM_FREE} MiB"
 echo -n "Used : "
 RAM_USED=$(top -b -n 1 | grep "MiB Mem" | cut -d ',' -f3 | awk '{print $1}')
 echo "${RAM_USED} MiB"
+echo -n "Free : "
+RAM_FREE=$(top -b -n 1 | grep "MiB Mem" | cut -d ',' -f2 | awk '{print $1}')
+echo -e "${RAM_FREE} MiB\n"
+echo "-----DISK USAGE------"
+echo -n "Total : "
+DISK_TOTAL=$(df -h | awk '$6 == "/"' | awk '{print $2}')
+echo "${DISK_TOTAL}"
+echo -n "Used : "
+DISK_USED=$(df -h | awk '$6 == "/"' | awk '{print $3}')
+echo "${DISK_USED}"
+echo -n "Free : "
+DISK_FREE=$(df -h | awk '$6 == "/"' | awk '{print $4}')
+echo -e "${DISK_FREE}\n"
