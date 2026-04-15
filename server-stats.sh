aad15394f378aebfd7c27e42f9e4c2170c44cca5
JescAude <jessica.moussougan@epitech.eu>
2026-04-15 16:54:04 +0100

    feat :sparkles: : added displaying of CPU usage, total/free/used RAM with no prettiness

---
+++ server-stats.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+##
+## EPITECH PROJECT, 2026
+## linux-system-monitor
+## File description:
+## server-stats
+##
+
+echo -e "---------------------------SERVER INFO-----------------------------\n"
+echo "----CPU USAGE----"
+CPU_USAGE=$(top -bn1 | grep Cpu | cut -d',' -f4 | awk '{print $1}')
+RES_1=$(echo "100 - ${CPU_USAGE}" | bc)
+echo -e "${RES_1} %\n"
+echo "-----RAM USAGE--------"
+echo -n "Total : "
+RAM_TOTAL=$(top -b -n 1 | grep "MiB Mem" | cut -d ',' -f1 | awk '{print $4}')
+echo "${RAM_TOTAL} MiB"
+echo -n "Free : "
+RAM_FREE=$(top -b -n 1 | grep "MiB Mem" | cut -d ',' -f2 | awk '{print $1}')
+echo "${RAM_FREE} MiB"
+echo -n "Used : "
+RAM_USED=$(top -b -n 1 | grep "MiB Mem" | cut -d ',' -f3 | awk '{print $1}')
+echo "${RAM_USED} MiB"
