7368ef8f3d35fda912359493af743b8ff7b29890
JescAude <jessica.moussougan@epitech.eu>
2026-04-16 16:47:40 +0100

    feat :sparkles: : added displaying of the top 5 processes by memory usage and improved the commands used for both top 5 by CPU and memory by using directly '--sort' option of ps command

--- server-stats.sh
+++ server-stats.sh
@@ -52,7 +52,15 @@ DISK_FREE=$(df -h | awk '$6 == "/"' | awk '{print $4}')
 echo -e "${DISK_FREE}\n"
 
 # Top 5 processes by CPU usage
-echo "-------------- Top 5 processes by CPU usage---------------"
-echo "    PID PROCESS         %CPU"
-TOP5_CPU=$(ps -eo pid,comm,%cpu | tail -n +2 | sort -k3 -nr | head -n 5)
+# echo "    PID PROCESS         %CPU"
+# ps -eo pid,comm,%cpu | tail -n +2 | sort -k3 -nr | head -n 5)
+echo "--------------- Top 5 processes by CPU usage ---------------"
+TOP5_CPU=$(ps -eo pid,comm,%cpu --sort=-%cpu | head -n 6)
 echo -e "${TOP5_CPU}\n"
+
+# Top 5 processes by memory usage
+# Another commands combination for the same result is : 
+# ps -eo pid,comm,%mem | tail -n +2 | sort -k3 -nr | head -n 5)
+echo "---------------- Top 5 processes by MEM usage ---------------"
+TOP5_MEM=$(ps -eo pid,comm,%mem --sort=-%mem | head -n 6)
+echo -e "${TOP5_MEM}\n"
