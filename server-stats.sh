c8bc155f5ecc71a9da74f162eac32b4380f52e2f
JescAude18 <jessicamoussougan@gmail.com>
2026-04-17 11:48:13 +0100

    feat :lipstick: : added colors and style to improve user interface

--- server-stats.sh
+++ server-stats.sh
@@ -7,19 +7,34 @@
 ## server-stats
 ##
 
-echo -e "---------------------------SERVER INFO-----------------------------\n"
+RESET="\033[0m"
+RED_B="\033[41m"
+GREEN_B="\033[42m"
+YELLOW_B="\033[43m"
+BLUE_B="\033[44m"
+BLUE="\033[34m"
+PINK_B="\033[35m"
+CYAN_B="\033[46m"
+
+echo -e "${PINK_B}=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=${RESET}"
+echo -e "${PINK_B}=                               SYSTEM INFO                               =${RESET}"
+echo -e "${PINK_B}=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=${RESET}\n"
+
+echo "Welcome to this system monitor ! It displays statistics on the CPU, RAM, disk, the" 
+echo " top five (05) processes by RAM and CPU, the os version, kernel, logged in users, "
+echo -e "architecture, uptime and load average. Thanks for trying it ♥️ .\n"
 
 # CPU usage
-echo "----CPU USAGE----"
+echo -e "${GREEN_B}------------------------- CPU USAGE -------------------------${RESET}\n"
 CPU_USAGE=$(top -bn1 | grep Cpu | cut -d',' -f4 | awk '{print $1}')
 RES_1=$(echo "100 - ${CPU_USAGE}" | bc)
-echo -e "${RES_1} %\n"
+echo -e "CPU usage : ${RES_1} %\n"
 
 # RAM usage
-echo "-----RAM USAGE--------"
+echo -e "${BLUE_B}------------------------- RAM USAGE -------------------------${RESET}\n"
 
 # Total RAM
-echo -n "Total : "
+echo -n "Total: "
 RAM_TOTAL=$(top -b -n 1 | grep "MiB Mem" | cut -d ',' -f1 | awk '{print $4}')
 echo "${RAM_TOTAL} MiB"
 
@@ -34,10 +49,10 @@ RAM_FREE=$(top -b -n 1 | grep "MiB Mem" | cut -d ',' -f2 | awk '{print $1}')
 echo -e "${RAM_FREE} MiB\n"
 
 # Disk usage
-echo "-----DISK USAGE------"
+echo -e "${YELLOW_B}------------------------- DISK USAGE ------------------------${RESET}\n"
 
 # Total disk
-echo -n "Total : "
+echo -n "Total: "
 DISK_TOTAL=$(df -h | awk '$6 == "/"' | awk '{print $2}')
 echo "${DISK_TOTAL}"
 
@@ -54,36 +69,36 @@ echo -e "${DISK_FREE}\n"
 # Top 5 processes by CPU usage
 # echo "    PID PROCESS         %CPU"
 # ps -eo pid,comm,%cpu | tail -n +2 | sort -k3 -nr | head -n 5)
-echo "--------------- Top 5 processes by CPU usage ---------------"
+echo -e "${RED_B}---------------- Top 5 processes by CPU usage ---------------${RESET}\n"
 TOP5_CPU=$(ps -eo pid,comm,%cpu --sort=-%cpu | head -n 6)
 echo -e "${TOP5_CPU}\n"
 
 # Top 5 processes by memory usage
 # Another commands combination for the same result is : 
 # ps -eo pid,comm,%mem | tail -n +2 | sort -k3 -nr | head -n 5)
-echo "---------------- Top 5 processes by MEM usage ---------------"
+echo -e "${RED_B}---------------- Top 5 processes by MEM usage ---------------${RESET}\n"
 TOP5_MEM=$(ps -eo pid,comm,%mem --sort=-%mem | head -n 6)
 echo -e "${TOP5_MEM}\n"
 
 # Bonus
-echo -e "-------------------------- Extra infos -----------------------------\n"
+echo -e "${CYAN_B}------------------------ Extra infos ------------------------${RESET}\n"
 # OS Version
-OS_VERSION=$(hostnamectl | grep "Operating System")
-echo -e "${OS_VERSION}\n"
+OS_VERSION=$(hostnamectl | grep "Operating System" | cut -d ':' -f2)
+echo -e "${BLUE}Operating System :${RESET}${OS_VERSION}\n"
 # Kernel
 KERNEL=$(hostnamectl | grep "Kernel" | cut -d ':' -f2)
-echo -e "Kernel: ${KERNEL}\n"
+echo -e "${BLUE}Kernel           :${RESET}${KERNEL}\n"
 # Architecture
 ARCHITECTURE=$(hostnamectl | grep "Architecture" | cut -d ':' -f2)
-echo -e "Architecture: ${ARCHITECTURE}\n"
+echo -e "${BLUE}Architecture     :${RESET}${ARCHITECTURE}\n"
 # Uptime
 UPTIME=$(uptime -p | awk '{ for (i=2;i<=NF;i++) printf "%s%s", $i, (i<NF ? OFS : ORS);}')
 SINCE=$(uptime -s)
-echo -e "Uptime: ${UPTIME} since ${SINCE}\n"
+echo -e "${BLUE}Uptime           :${RESET} ${UPTIME} since ${SINCE}\n"
 # Load average
 LOAD=$(uptime | awk '{for(i=8;i<=NF;i++) printf "%s%s", $i, (i<NF ? OFS : ORS);}')
-echo -e "Load average (1 minute, 5 minutes, 15 minutes): ${LOAD}\n"
+echo -e "${BLUE}Load average     : ${RESET}${LOAD}\n"
 # Logged in users
 LOGGED=$(who)
-echo "Logged in users:"
+echo -e "${BLUE}Logged in users  :${RESET}"
 echo -e "${LOGGED}\n"
